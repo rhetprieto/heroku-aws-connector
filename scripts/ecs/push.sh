4122bfdd5526664712ddcceef32ec9e395d6c118
rhetprieto <rhetprieto@gmail.com>
2021-12-10 11:32:14 -0600

    Fix on push script

--- scripts/ecs/push.sh
+++ scripts/ecs/push.sh
@@ -5,17 +5,17 @@ set -o pipefail
 
 # This script pushes the latest build to ECR.
 
-REPOSITORY_NAME=${REPOSITORY_NAME:heroku-aws-connector}
-REGION=${REGION:-us-east-1}
-NAME=${NAME:-heroku-aws-connector}
-VERSION=${VERSION:-$(cat ../../version.txt)}
+NAME="heroku-aws-connector"
+CLUSTER_NAME="heroku-aws-connector"
+REGION="us-east-1"
+VERSION=$(cat ../../version.txt)
 
 echo Logging into ECR ...
 # Login to docker
 $(aws ecr get-login --region $REGION | sed -e 's/-e none//g')
 
 ## Fetch repository name
-REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
+REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
 echo Found ECR repository: $REPOSITORY_URI
 
 CONNECTOR_IMAGE=$REPOSITORY_URI:$NAME:$VERSION
